b6e51219f604a3c24c68effa043223acb7cb2ead
CLbox <cleibox@gmail.com>
2021-04-01 18:03:29 -0400

    part 3

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -1,6 +1,6 @@
 // Throughout this project, the use of data structures are not permitted such as methods like .split and .toCharArray
-// hihi
-// this is me testing how github works again part 2
+// hihi dklfsdjlkfdsklfjklsdjflkds
+
 
 import java.util.Scanner;
 // More packages may be imported in the space below
